457d71707a1c6d6db04b7f6493fa5b49cebcf101
Alexandre <ale.developer72@gmail.com>
2025-02-10 11:30:02 -0300

    Atualização

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -1,11 +1,22 @@
 
+import Image from 'next/image';
 import styles from './Reviews.module.scss'
+import Stars from '@/Ui/Stars';
 
 const Reviews = () => {
     return (
         <section className={styles.reviews}>
-            reviews
-        </section>
+            <Image className={styles.image} src='/graos.png' alt='graos de café' width={425} height={250} />
+            <h2 className='title light-bg'>O que falam da gente</h2>
+            <div className={styles.review}>
+                <Image className={styles.avatar}
+                src='/avatar1.png' alt='avatar' width={120} height={120} />
+                <h3>nome</h3>
+                <h4>profissão</h4>
+                <Stars stars={5} />
+                <p>avaliação</p>
+            </div>
+            </section>
     )
 }
 
